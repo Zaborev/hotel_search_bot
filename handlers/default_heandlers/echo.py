50d6eb317ea8472fbd583b29eb072581b1a03d7f
Zaborev Alexandr <complus@list.ru>
2022-10-24 13:49:13 +0300

    Попытка №2

--- handlers/default_heandlers/echo.py
+++ handlers/default_heandlers/echo.py
@@ -1,4 +1,5 @@
 from telebot.types import Message
+import random
 
 from loader import bot
 
@@ -6,5 +7,12 @@ from loader import bot
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
-    bot.reply_to(message, "Эхо без состояния или фильтра.\nСообщение:"
-                          f"{message.text}")
+    hi = ('Привет', 'привет', 'здравствуйте', 'Здорово', 'хай', 'Хай')
+    replicas = ('Привет, чем я могу Вам помочь?', 'Добрый день, давайте я помогу Вам подобрать отель?',
+                'Добрый день, готов услышать ваш вопрос!',
+                'Привет, сегодня отличный день, чтобы отправиться в поездку! Начнём подбирать отель?')
+    if message.text in hi:
+        random_replica = random.randint(0, len(replicas))
+        bot.send_message(message.from_user.id, replicas[random_replica])
+    else:
+        bot.send_message(message.from_user.id, "Я Вас не понимаю. Напишите /help чтобы узнать список команд")
